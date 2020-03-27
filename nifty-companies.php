4a5f591d17a037bf51c14e0098d77010fc0e6735
Tanuj Dey <tanujdey7@gmail.com>
2020-03-27 22:15:58 +0530

    v2

--- nifty-companies.php
+++ nifty-companies.php
@@ -23,6 +23,7 @@ if (isset($_SESSION["username"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <!-- <link rel="stylesheet" href="styles.css"> -->
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
 
     <!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" /> -->
@@ -44,6 +45,7 @@ if (isset($_SESSION["username"])) {
             box-sizing: border-box;
             margin: 0;
             padding: 0;
+            font-family: 'Raleway', Arial, Helvetica, sans-serif;
         }
 
         header {
@@ -61,6 +63,7 @@ if (isset($_SESSION["username"])) {
         .nav__links {
             list-style: none;
             display: flex;
+            text-transform: uppercase;
         }
 
         .nav__links a,
@@ -259,22 +262,16 @@ if (isset($_SESSION["username"])) {
             padding-top: 20px;
         }
 
-        * {
-            box-sizing: border-box;
-        }
-
-        body {
-            margin: 0;
-        }
-
         p {
             max-width: 600px;
             margin: 0 auto 1rem;
         }
+
         @media(max-width: 700px) {
             footer .inner-footer {
                 width: 90%;
             }
+
             .inner-footer .footer-third {
                 width: 100%;
                 margin-bottom: 30px;
@@ -324,8 +321,7 @@ if (isset($_SESSION["username"])) {
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
-                                    <center>
-                                        <table class="table table-hover text-center">
+                                        <table class="table table-hover">
                                             <thead class=" text-primary">
                                                 <th>
                                                     Serial Number
@@ -392,7 +388,6 @@ if (isset($_SESSION["username"])) {
 
                                             </tbody>
                                         </table>
-                                    </center>
                                 </div>
                             </div>
                         </div>
@@ -434,23 +429,7 @@ if (isset($_SESSION["username"])) {
     <script src="resources/js/core/jquery.min.js"></script>
     <script src="resources/js/core/popper.min.js"></script>
     <script src="resources/js/core/bootstrap-material-design.min.js"></script>
-    <script src="resources/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-    <script src="resources/js/plugins/moment.min.js"></script>
-    <script src="resources/js/plugins/sweetalert2.js"></script>
-    <script src="resources/js/plugins/jquery.validate.min.js"></script>
-    <script src="resources/js/plugins/jquery.bootstrap-wizard.js"></script>
-    <script src="resources/js/plugins/bootstrap-selectpicker.js"></script>
-    <script src="resources/js/plugins/bootstrap-datetimepicker.min.js"></script>
-    <script src="resources/js/plugins/jquery.dataTables.min.js"></script>
-    <script src="resources/js/plugins/bootstrap-tagsinput.js"></script>
-    <script src="resources/js/plugins/jasny-bootstrap.min.js"></script>
-    <script src="resources/js/plugins/fullcalendar.min.js"></script>
-    <script src="resources/js/plugins/jquery-jvectormap.js"></script>
-    <script src="resources/js/plugins/nouislider.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-    <script src="resources/js/plugins/arrive.min.js"></script>
-    <script src="resources/js/plugins/chartist.min.js"></script>
-    <script src="resources/js/plugins/bootstrap-notify.js"></script>
     <script src="resources/js/material-dashboard.js?v=2.1.2" type="text/javascript"></script>
     <script src="resources/demo/demo.js"></script>
     <script>
