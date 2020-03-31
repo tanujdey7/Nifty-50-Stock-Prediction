18fc860fd8b439b3abeb16527c72cb683140103d
Maruf <maruf.glsica@gmail.com>
2020-03-31 23:35:08 +0530

    admin_panel

--- admin_panel/includes/connect.php
+++ admin_panel/includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-		$link = mysqli_connect("localhost", "root", "root");
+		$link = mysqli_connect("localhost", "root", "");
 		mysqli_select_db($link, "predictor");  
 		mysqli_query($link, "SET CHARACTER SET utf8");
 ?>
--- admin_panel/try.py
+++ admin_panel/try.py
@@ -3,6 +3,16 @@ from datetime import datetime
 from datetime import date
 import sys
 
-ibm = DataReader(sys.argv[1] + ".NS",  'yahoo', datetime(2012, 1, 1), datetime.now())
-str = "../stock_data/" + sys.argv[1] + ".csv"
-ibm.to_csv(str)
+# ibm = DataReader(sys.argv[1] + ".NS",  'yahoo', datetime(2012, 1, 1), datetime.now())
+# str = "../stock_data/" + sys.argv[1] + ".csv"
+# ibm.to_csv(str)
+
+if(sys.argv[1] == "^NSEI"):
+    ibm = DataReader("^NSEI",  'yahoo', datetime(2012, 1, 1), datetime.now())
+    str = "../stock_data/" + sys.argv[1] + ".csv"
+    ibm.to_csv(str)
+
+if(sys.argv[1] != "NSEI"):
+    ibm = DataReader(sys.argv[1] + ".NS",  'yahoo', datetime(2012, 1, 1), datetime.now())
+    str = "../stock_data/" + sys.argv[1] + ".csv"
+    ibm.to_csv(str)
